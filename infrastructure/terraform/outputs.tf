973f70f1a438afdc16078ba7715cd75f66128894
Bogdan Mihai <bogdan-eugen.mihai@klarna.com>
2020-02-09 00:36:50 +0100

    Only required outputs are displayed

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -1,19 +1,7 @@
-output movies_stats_api_gw_id {
-  value = module.movies_stats_api_gw.id
-}
-
-output movies_stats_api_gw_execution_arn {
-  value = module.movies_stats_api_gw.execution_arn
-}
-
-output movies_stats_api_deployment_id {
-  value = module.movies_stats_api_deployment.id
-}
-
-output movies_stats_api_deployment_execution_arn {
-  value = module.movies_stats_api_deployment.execution_arn
-}
-
 output movies_stats_api_deployment_execution_invoke_url {
   value = module.movies_stats_api_deployment.invoke_url
+}
+
+output movies_bucket {
+  value = module.movies_bucket.name
 }
